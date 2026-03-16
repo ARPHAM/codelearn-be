4dc23bedf0aba5a9484d50f1181a41be887ee981
ARPHAM <oaichua42@gmail.com>
2026-03-16 16:08:23 +0700

    đa file

--- src/modules/execution/execution.gateway.ts
+++ src/modules/execution/execution.gateway.ts
@@ -3,7 +3,8 @@ import { Server } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
-    origin: '*',
+    origin: ['http://localhost:3000'],
+    credentials: true,
   },
 })
 export class ExecutionGateway {
